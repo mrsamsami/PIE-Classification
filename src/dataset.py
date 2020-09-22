a87b3a7272bbe0d6b9994e180f2c6f9155377a58
mrsamsami <mohammad.r.samsami@gmail.com>
2020-09-22 17:30:18 +0330

    Update dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -2,7 +2,8 @@ import json
 import sys
 import pandas as pd
 
-threshold = sys.argv[1]
+threshold = float(sys.argv[1])
+biased = False if sys.argv[2] == 'fair' else True
 
 with open('../dataset/raw/MAGPIE_filtered_split_random.jsonl', 'r') as file:
     list = list(file)
@@ -18,12 +19,21 @@ df = df[df['confidence'] >= threshold]
 df = df.drop(['document_id', 'id', 'judgment_count', 'label_distribution', 'confidence', 'non_standard_usage_explanations', 'offsets', 'sentence_no', 'variant_type'], axis = 1)
 df['label'] = df.label.apply(lambda x: 1 if x == 'i' else 0)
 
-# Split dataset into train, dev, and test
 train = df[df.split.eq('training')].drop(['split'], axis = 1)
 dev = df[df.split.eq('development')].drop(['split'], axis = 1)
 test = df[df.split.eq('test')].drop(['split'], axis = 1)
 
-# Save dataset into csv files
+def unbias(df):
+    frac = (len(df[df['label'] == 1]) - len(df[df['label'] == 0])) / len(df[df['label'] == 1])
+    df1 = df[df['label'] == 1].sample(frac = 1 - frac)
+    df2 = df[df['label'] == 0]
+    return pd.concat([df1, df2])
+
+if not biased:
+    train = unbias(train)
+    test = unbias(test)
+    dev = unbias(dev)
+
 train.to_csv('../dataset/processed/train.csv', index = False)
 test.to_csv('../dataset/processed/test.csv', index = False)
 dev.to_csv('../dataset/processed/dev.csv', index = False)
